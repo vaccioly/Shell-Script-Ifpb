b319d9c2f3ba217f4989522113abbbc142c07bc9
Vinicius Accioly <vaccioly@gmail.com>
2019-10-30 21:18:24 -0300

    Exercicio do dia 30-10-2019

--- 09-Estruturas de Repetição/Exercicios/reveilon.sh
+++ 09-Estruturas de Repetição/Exercicios/reveilon.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-
-for (( i=0; i<10; i++)); do
+# Versão incrementando +1
+echo "Contagem regressiva..."
+for (( i=0; i<=10; i++)); do
 	echo "$((10 - i))"
 	sleep 1
 done
+echo "Feliz Ano Novo!!!"
+
+# Versão 2 decrementando -1
+
+echo "Contagem regressiva ... "
+for ((i=10; i>=0; i--)); do
+	echo "$i $(sleep 1)"
+done
+echo "Feliz Ano Novo!!!"
