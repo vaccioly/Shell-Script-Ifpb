f826a0f1f393ca4f613603cfb6611212cecdea1b
Vinicius Accioly <vaccioly@gmail.com>
2019-11-06 22:09:57 -0300

    Renomeados

--- 01-Introducao e Conceitos Basicos/exemplos/hello2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Hello, World"
-
-read -p "Qual o seu nome? " nome
-echo "Prazer em te conhecer, ${nome}!"
--- 01-Introducao e Conceitos Basicos/exercicios/exercicio01.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Hello, World"
-
-read -p "Qual o seu nome? " nome
-echo -e "Prazer em te conhecer, ${nome}! Hoje é $(date +%d) $(date +%m) $(date +%y),$(date +%A) "
